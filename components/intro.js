09faafc3e0e9f2d8bcb6da3097b4bc7f17dcc560
jordan gilliam <gilliamja.te@gmail.com>
2021-02-14 23:16:46 +0300

    rougher draft

--- components/intro.js
+++ components/intro.js
@@ -1,18 +1,18 @@
-import {DOM_NAME, ARTIST_NAME} from '../lib/constants'
+import {DOM_NAME, HEADING_NAME} from '../lib/constants'
 
 export default function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        {ARTIST_NAME}
+      <h1 className="fonttext-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+        {HEADING_NAME}
       </h1>
       <h3 className="text-center md:text-left text-4xl mt-5 md:pl-8">
-        {DOM_NAME}.
         <a
           href="https://goo.gl/maps/AWmhCibfQLEURBuRA"
-          className="underline hover:text-success duration-200 transition-colors"
-        ></a>{' '}
-        location
+          className="hover:text-red-500 duration-200 transition-colors hover:underline "
+        >
+          {DOM_NAME}.location
+        </a>{' '}
       </h3>
     </section>
   )
--- components/meta.js
+++ components/meta.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import {DOM_NAME, HOME_OG_IMAGE_URL} from '../lib/constants'
+import {DOM_NAME} from '../lib/constants'
 
 export default function Meta() {
   return (
@@ -32,11 +32,8 @@ export default function Meta() {
       <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
       <meta name="theme-color" content="#000" />
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
-      <meta
-        name="description"
-        content={`Using the right hemisphere ${DOM_NAME}.`}
-      />
-      <meta property="og:image" content={HOME_OG_IMAGE_URL} />
+      <meta name="description" content={`-> hemisphere ${DOM_NAME}.`} />
+      <meta property="og:image" content="/meta/the-question.jpg" />
     </Head>
   )
 }
